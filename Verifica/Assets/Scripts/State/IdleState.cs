478c2a762e0a019d4ad090520b3ada5a24872e2c
Maikx <mikeybee2000@gmail.com>
2020-03-17 13:16:39 +0100

    Added Codes to StateMachine States

--- Verifica/Assets/Scripts/State/IdleState.cs
+++ Verifica/Assets/Scripts/State/IdleState.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleState : MonoBehaviour
+
+public class IdleState : StateMachineBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        //Farccio qualcosa inerente al Main menu : ES : Attivo Ui Menu
     }
 
-    // Update is called once per frame
-    void Update()
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+
     }
-}
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Faccio qualcosa : Es : Disattivo Ui Main Menu e attivo UI Gameplay
+    }
+
+   //// OnStateMove is called right after Animator.OnAnimatorMove()
+   // override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+   // {
+   //     // Implement code that processes and affects root motion
+   // }
+
+// //OnStateIK is called right after Animator.OnAnimatorIK()
+// override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+// {
+//     // Implement code that sets up animation IK (inverse kinematics)
+ }
